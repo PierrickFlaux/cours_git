6d70a4231ab4b3ff650ccb1a4f6e9c242a063e76
pierrick flaux <pierrick.flaux.pro@gmail.com>
2020-04-03 11:57:50 +0200

    implémentation de la classe Utilisateur

--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -6,4 +6,40 @@
  */
 public class Utilisateur {
 
+		/*_________________ props ______________________*/
+		private int id;
+		private String nom;
+		
+		
+		
+		/*_________________ ctors ______________________*/
+		
+		public Utilisateur() {}
+		
+		/*_________________ méthodes ______________________*/
+		
+		/*_________________ get/set ______________________*/
+	
+		public int getId() {
+			return id;
+		}
+
+
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+
+
+		public String getNom() {
+			return nom;
+		}
+
+
+
+		public void setNom(String nom) {
+			this.nom = nom;
+		}
+		
 } // end class
